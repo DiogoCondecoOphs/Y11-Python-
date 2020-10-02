3bc6f677c24b70cf91086b2827e00ff50f8faf0f
DiogoCondecoOphs <71497319+DiogoCondecoOphs@users.noreply.github.com>
2020-10-02 15:27:59 +0100

    Update Python_challenge2.py

--- Python_challenge2.py
+++ Python_challenge2.py
@@ -1,7 +1,9 @@
+#This code is used to create details for a username signing up for a game service for the first time (really basic)
 print("Welcome to [insert game name]")
 age= int(input("Please enter your age: "))
 sex = str(input("Please enter your gender: "))
 email = str(input("Please enter your email address: "))
+username= str(input("Please enter your username :"))
 #This initialises the data of the player
 
 print(age)
@@ -27,11 +29,21 @@ while email_conf != "y":
      email=str(input("Please re-enter your Email :"))
      print(email)
      email_conf =str(input("Is this your Email? y/n "))
-print("""Here are your details 
+     
+print(username)
+username_conf =str(input("Is this your username? y/n "))
+while username_conf != "y":
+     username=int(input("Please re-enter your username :"))
+     print(username)
+     username_conf =str(input("Is this your username? y/n "))
+
+print("""Here are your details
       age is""",age,"""
+      username is""",username,"""
       email is""",email,"""
       gender is """,sex,)
 print("Thank you for regsitering to [insert game name], have fun")     
 exit
 #data is registered for the user correctly and exits from the screen
+
      
