c26c1b9fd7cb2de6b2e1b40c66f7c0aa180f1a8d
DiogoCondecoOphs <71497319+DiogoCondecoOphs@users.noreply.github.com>
2021-02-05 11:30:35 +0000

    Create WHILELOOPchallenges03.py

---
+++ WHILELOOPchallenges03.py
@@ -0,0 +1,10 @@
+num1= int(input("please enter a number "))
+total = num1
+val= "y"
+
+while val == "y":
+    num2 = int(input("please enter another number "))
+    total = total + num2
+    val=str(input("would you like to add another number y/n "))
+
+print("total was",total)
