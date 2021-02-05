23aac89d909997781f0ba279c4e7aaf65d45ab45
DiogoCondecoOphs <71497319+DiogoCondecoOphs@users.noreply.github.com>
2021-02-05 11:33:52 +0000

    Create WHILELOOPchallenges04.py

---
+++ WHILELOOPchallenges04.py
@@ -0,0 +1,10 @@
+count = 0
+valid = "y"
+
+while valid == "y":
+    name = str(input("who would you like to invite to the party "))
+    print(name,"has now been invited")
+    count = count + 1
+    valid = str(input("Would you like to invite someone else? y/n "))
+
+print(count,"person/people were invited")
