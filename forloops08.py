9e059d0f133ae4e0fc26170f63235c36e9085b80
DiogoCondecoOphs <71497319+DiogoCondecoOphs@users.noreply.github.com>
2021-02-08 15:06:16 +0000

    Update forloops08.py

--- forloops08.py
+++ forloops08.py
@@ -2,7 +2,7 @@ dic = str(input("what direction do you want to count u/d?: ")).lower()
 
 if dic == "u":
     tnum = int(input("Please enter the number you want to count up to: "))
-    for i in range(1,tnum):
+    for i in range(0,tnum):
         print(i)
 elif dic == "d":
     dnum = int(input("please enter a number below 20 to count down to: "))
