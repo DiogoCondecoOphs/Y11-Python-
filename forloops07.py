53eee7a6be86ddfc1cf5884ec7b2efd4131ffdcd
DiogoCondecoOphs <71497319+DiogoCondecoOphs@users.noreply.github.com>
2021-02-08 14:47:47 +0000

    Create forloops07.py

---
+++ forloops07.py
@@ -0,0 +1,13 @@
+total = 0
+for i in range(0,6):
+    num = int(input("please enter a number"))
+    print("")
+    val = str(input("Would you like to include that number? y/n ")).lower()
+    
+    if val == "y":   
+     total = total + num
+
+    else:
+        print("")
+
+print(total)
