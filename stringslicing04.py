daf82d25977e90c22d0651aa388ae49c6da4caba
DiogoCondecoOphs <71497319+DiogoCondecoOphs@users.noreply.github.com>
2021-02-12 09:43:50 +0000

    Create stringslicing04.py

---
+++ stringslicing04.py
@@ -0,0 +1,9 @@
+nrhyme = str(input("Please enter the first line of a nursery rhyme: "))
+length = len(nrhyme)
+
+print("This has",length,"letters in it")
+
+num1 = int(input("please enter a starting number: "))
+num2 = int(input("please enter an ending number: "))
+
+str(print(nrhyme[num1:num2]))
