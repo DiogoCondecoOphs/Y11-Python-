96ff795c4c26e7130a6673370309e8b0bd782ff7
DiogoCondecoOphs <71497319+DiogoCondecoOphs@users.noreply.github.com>
2021-02-12 09:50:05 +0000

    Create stringslicing06.py

---
+++ stringslicing06.py
@@ -0,0 +1,13 @@
+fname = str(input("Please enter your first name: "))
+length = len(fname)
+
+if length > 5:
+    lname = str(input("Please enter your last name: "))
+    fname = fname.upper()
+    fullname = fname + lname
+    print(fullname)
+
+else:
+    fname = fname.lower()
+    print(fname)
+    
