5a930bd5d1f0187839f83d2766f7ae70fffdada3
DiogoCondecoOphs <71497319+DiogoCondecoOphs@users.noreply.github.com>
2021-02-12 10:14:31 +0000

    Create stringslicing07.py

---
+++ stringslicing07.py
@@ -0,0 +1,32 @@
+fname = str(input("please enter your name: "))
+
+if fname[0] == "a":
+    
+    print(fname.strip("a"))
+
+
+elif fname[0] == "e":
+    
+    
+     print(fname.strip("e"))
+
+     
+elif fname[0] == "i":
+    
+    
+     print(fname.strip("i"))
+
+elif fname[0] == "o":
+    
+    
+     print(fname.strip("o"))
+
+elif fname[0] == "u":
+
+    
+     print(fname.strip("u"))
+
+    
+    
+else:
+    print(fname)
