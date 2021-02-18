fbb4e68d40e68d3fffba73beaae072f71b1e68a4
DiogoCondecoOphs <71497319+DiogoCondecoOphs@users.noreply.github.com>
2021-02-18 19:31:53 +0000

    Create 2017NEA_Task2.py

---
+++ 2017NEA_Task2.py
@@ -0,0 +1,67 @@
+print("Welcome to the registration calculator")
+
+val = "y"
+
+while val == "y":
+
+ email = str(input("Please enter your email: "))
+ print(" ")
+
+ skill = str(input("Please enter your skill E/C ")).upper()
+ print(" ")
+
+ country = str(input("Please enter your country of residence UK,US,AU: ")).upper()
+ print(" ")
+
+ fee = 10.00
+ 
+ e_fee = 35.00
+
+ c_fee = 20.00
+
+ total = 0.00
+
+ if country == "UK":
+     total = fee
+
+     if skill == "E":
+         total = total + e_fee
+         print("total is",total,"GBP")
+
+     else:
+        total = total + c_fee
+        print("total is",total,"GBP")
+
+ elif country == "US":
+     total = fee
+
+     if skill == "E":
+         total = total + e_fee
+         total = total * 1.5
+         print("total is",total,"USD")
+
+     else:
+        total = total + c_fee
+        total = total *1.5
+        print("total is",total,"USD")
+
+ elif country == "AU":
+     total = fee
+
+     if skill == "E":
+         total = total + e_fee
+         total = total * 2.00
+         print("total is",total,"AUD")
+
+     else:
+        total = total + c_fee
+        total = total * 2.0
+        print("total is",total,"AUD")
+
+ else:
+     print("Please enter a valid country")
+ print(" ")
+ val = str(input("Would you like to calculate another fee? y/n ")).lower()
+
+print("Thank you for using our fee calculator")
+         
