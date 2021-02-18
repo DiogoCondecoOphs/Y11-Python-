f5f8e2171b9f4dc773cc1ccce2fe048793bd4511
DiogoCondecoOphs <71497319+DiogoCondecoOphs@users.noreply.github.com>
2021-02-18 19:32:17 +0000

    Create 2017NEA_Task1.py

---
+++ 2017NEA_Task1.py
@@ -0,0 +1,46 @@
+
+print("""
+          ######################
+            League registration
+          ######################""")
+print(" ")
+
+val = "n"
+
+while val == "n":
+    
+ fname = str(input("Please enter your first name: "))
+
+ lname= str(input("Please enter your last name: "))
+
+ nick = str(input("Please enter your nickname: "))
+
+ email = str(input("Please enter your email: "))
+ 
+
+ skill = str(input("Please enter your skill level E/C ")).upper()
+
+ if skill == "E":
+     print("""
+              Expert""")
+     print(fname)
+     print(lname)
+     print(nick)
+     print(email)
+     print(" ")
+
+     val = str(input("Are your details correct? y/n ")).lower()
+
+ else:
+     print("""
+              Casual""")
+     print(fname)
+     print(lname)
+     print(nick)
+     print(email)
+     print(" ")
+
+     val = str(input("Are your details correct? y/n ")).lower()
+
+
+print("Thank you for your entry")
