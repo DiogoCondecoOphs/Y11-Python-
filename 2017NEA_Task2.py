545b0feb862d76538c1752110f9fb6ed015acc7f
DiogoCondecoOphs <71497319+DiogoCondecoOphs@users.noreply.github.com>
2021-02-19 16:33:58 +0000

    Update 2017NEA_Task2.py

--- 2017NEA_Task2.py
+++ 2017NEA_Task2.py
@@ -1,67 +1,70 @@
-print("Welcome to the registration calculator")
+def calculator():
 
-val = "y"
+    print("Welcome to the registration calculator")
 
-while val == "y":
+    val = "y"
 
- email = str(input("Please enter your email: "))
- print(" ")
+    while val == "y":
 
- skill = str(input("Please enter your skill E/C ")).upper()
- print(" ")
+     email = str(input("Please enter your email: "))
+     print(" ")
 
- country = str(input("Please enter your country of residence UK,US,AU: ")).upper()
- print(" ")
+     skill = str(input("Please enter your skill E/C ")).upper()
+     print(" ")
 
- fee = 10.00
- 
- e_fee = 35.00
+     country = str(input("Please enter your country of residence UK,US,AU: ")).upper()
+     print(" ")
 
- c_fee = 20.00
+     fee = 10.00
+     
+     e_fee = 35.00
 
- total = 0.00
+     c_fee = 20.00
 
- if country == "UK":
-     total = fee
+     total = 0.00
 
-     if skill == "E":
-         total = total + e_fee
-         print("total is",total,"GBP")
+     if country == "UK":
+         total = fee
+
+         if skill == "E":
+             total = total + e_fee
+             print("total is",total,"GBP")
+
+         else:
+            total = total + c_fee
+            print("total is",total,"GBP")
+
+     elif country == "US":
+         total = fee
+
+         if skill == "E":
+             total = total + e_fee
+             total = total * 1.5
+             print("total is",total,"USD")
+
+         else:
+            total = total + c_fee
+            total = total *1.5
+            print("total is",total,"USD")
+
+     elif country == "AU":
+         total = fee
+
+         if skill == "E":
+             total = total + e_fee
+             total = total * 2.00
+             print("total is",total,"AUD")
+
+         else:
+            total = total + c_fee
+            total = total * 2.0
+            print("total is",total,"AUD")
 
      else:
-        total = total + c_fee
-        print("total is",total,"GBP")
+         print("Please enter a valid country")
+     print(" ")
+     val = str(input("Would you like to calculate another fee? y/n ")).lower()
 
- elif country == "US":
-     total = fee
-
-     if skill == "E":
-         total = total + e_fee
-         total = total * 1.5
-         print("total is",total,"USD")
-
-     else:
-        total = total + c_fee
-        total = total *1.5
-        print("total is",total,"USD")
-
- elif country == "AU":
-     total = fee
-
-     if skill == "E":
-         total = total + e_fee
-         total = total * 2.00
-         print("total is",total,"AUD")
-
-     else:
-        total = total + c_fee
-        total = total * 2.0
-        print("total is",total,"AUD")
-
- else:
-     print("Please enter a valid country")
- print(" ")
- val = str(input("Would you like to calculate another fee? y/n ")).lower()
-
-print("Thank you for using our fee calculator")
+    print("Thank you for using our fee calculator")
          
+calculator()
